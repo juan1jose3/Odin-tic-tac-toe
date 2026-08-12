af911736e13ffd0799fcc8970998a3bcf09c08a2
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-08-12 15:04:14 -0500

    added winning logic

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,4 +1,3 @@
-
 function player(name, mark) {
   return {
     name,
@@ -13,6 +12,7 @@ function player(name, mark) {
     }
   };
 }
+
 
 const gameBoard = (() => {
   let board = [];
@@ -31,22 +31,25 @@ const gameBoard = (() => {
       return true;
     }
     
-    
-
     console.log("This spot is Filled!");
     return false;
   }
-    
+
   const getBoard = () => {
+    return board;
+  }
+  const showBoard = () => {
     console.log(
       `
+      
       ${board[0]} | ${board[1]} | ${board[2]} \n
       ${board[3]} | ${board[4]} | ${board[5]} \n
       ${board[6]} | ${board[7]} | ${board[8]} \n
+      
     `);
   };
 
-  return { crateGrid, getBoard, markBoard };
+  return { crateGrid, showBoard, markBoard, getBoard };
     
 })();
 
@@ -72,43 +75,60 @@ const gameController = (() => {
   let activePlayer = players[0];
   
 
-  const switchTurn = (() => {
+  const switchTurn = () => {
     if (activePlayer.turn === 1) {
       activePlayer = players[1];
     } else {
       activePlayer = players[0];
     }
     
-  });
+  };
 
-  const showTurn = (() => {
+  const showTurn = () => {
     console.log(`It's ${activePlayer.player.getPlayerName()}'s turn`);
-  });
+  };
+
+  const winningOptions = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    // Diagonals
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  let isWinner = () => {
+    const currentBoard = board.getBoard();
+    return winningOptions.some((winnOption => currentBoard[winnOption[0]] && currentBoard[winnOption[0]] === currentBoard[winnOption[1]] && currentBoard[winnOption[1]] === currentBoard[winnOption[2]]));
+  };
+
+    
   
-  const playRound = (() => {
+  const playRound = () => {
     showTurn();
     console.log(`Turn: ${currentTurn}`);
     
-    move = board.markBoard(0, activePlayer.player.getPlayerMark());
+    move = board.markBoard(2, activePlayer.player.getPlayerMark());
 
+    move = board.markBoard(4, activePlayer.player.getPlayerMark());
+    
+    move = board.markBoard(6, activePlayer.player.getPlayerMark());
+
+    console.log(`Is winner?: ${isWinner()}`)
+
+    
     if (move) {
       currentTurn++;
     }
     
-    board.getBoard();
-    switchTurn();
-    
-    showTurn();
-  
-    console.log(`Turn: ${currentTurn}`);
-    
-    move = board.markBoard(0, activePlayer.player.getPlayerMark());
-    board.getBoard();
-
-    if (move) {
-      currentTurn++;
-    }
-  });
+  };
 
     playRound();
 
